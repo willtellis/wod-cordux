83180010ad64b6be5d2fd70abae2900c35f2a887
willtellis <will.ellis@willowtreeapps.com>
2017-02-21 09:14:43 -0500

    Add Initialization and BrowsingState to AppState

--- WODCordux/AppState.swift
+++ WODCordux/AppState.swift
@@ -13,6 +13,21 @@ typealias Store = Cordux.Store<AppState>
 
 struct AppState: StateType {
     var route: Route = []
+    var initialization: Initialization = .uninitialized
+}
+
+enum Initialization {
+    case uninitialized
+    case initialized(BrowsingState)
+}
+
+struct BrowsingState {
+    var wods: [WodState] = []
+}
+
+struct WodState {
+    var name: String
+    var content: String
 }
 
 final class AppReducer: Reducer {
