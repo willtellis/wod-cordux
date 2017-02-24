864210ab5e3e39256be91c69adcb9e214d1b908e
willtellis <will.ellis@willowtreeapps.com>
2017-02-24 10:21:03 -0700

    Add table view to WodsViewController

--- WODCordux/Scenes/Wods/WodsViewController.swift
+++ WODCordux/Scenes/Wods/WodsViewController.swift
@@ -23,13 +23,52 @@ protocol WodsHandler {
 
 final class WodsViewController: UIViewController {
     
+    @IBOutlet var tableView: UITableView!
+    var viewModel: WodsViewModel = WodsViewModel()
     var handler: WodsHandler?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        navigationItem.title = NSLocalizedString("Wods", comment: "")
+    }
+        
     func inject(handler: WodsHandler) {
         self.handler = handler
     }
     
     func render(_ viewModel: WodsViewModel) {
-        // TODO WTE
+        self.viewModel = viewModel
+        tableView.reloadData()
+    }
+}
+
+extension WodsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.wodTableViewCellViewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: WodsTableViewCell.reusableIdentifier, for: indexPath) as! WodsTableViewCell
+        let cellViewModel = viewModel.wodTableViewCellViewModels[indexPath.row]
+        cell.render(cellViewModel)
+        return cell
+    }
+}
+
+
+final class WodsTableViewCell: UITableViewCell, ClassNameIdentifiable {
+    
+    @IBOutlet var title: UILabel!
+    @IBOutlet var content: UILabel!
+    
+    func render(_ viewModel: WodTableViewCellViewModel) {
+        title.text = viewModel.title
+        content.text = viewModel.content
     }
 }
